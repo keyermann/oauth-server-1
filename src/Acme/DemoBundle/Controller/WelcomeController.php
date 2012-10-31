87dacb2aacd87bd9d621e1b3c040e36df485a863
Robert Gruendler <r.gruendler@gmail.com>
2012-10-31 23:10:57 +0100

    added formtype, fixed endpoints

--- src/Acme/DemoBundle/Controller/WelcomeController.php
+++ src/Acme/DemoBundle/Controller/WelcomeController.php
@@ -2,17 +2,64 @@
 
 namespace Acme\DemoBundle\Controller;
 
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+
+use Symfony\Component\Form\FormInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 class WelcomeController extends Controller
 {
+    /**
+     * @Template()
+     */
     public function indexAction()
     {
-        /*
-         * The action's view can be rendered using render() method
-         * or @Template annotation as demonstrated in DemoController.
-         *
-         */
-        return $this->render('AcmeDemoBundle:Welcome:index.html.twig');
+        return array();
+    }
+
+    public function appsAction()
+    {
+        die('aha');
+    }
+
+    /**
+     * @Template()
+     */
+    public function createAppAction()
+    {
+        $defaultData = array('name' => 'app name');
+        $form = $this->createFormBuilder($defaultData)
+            ->add('name', 'text')
+            ->add('redirect_uri', 'url')
+            ->getForm();
+
+        if ($this->getRequest()->isMethod('POST')) {
+
+            $form->bind($this->getRequest());
+
+            if ($form->isValid()) {
+                $this->processForm($form);
+                return $this->redirect($this->generateUrl('_apps'));
+            }
+        }
+
+        return array('form' => $form->createView());
+    }
+
+
+    /**
+     * @param FormInterface $form
+     */
+    protected function processForm(FormInterface $form)
+    {
+        $data = $form->getData();
+        $clientManager = $this->get('fos_oauth_server.client_manager.default');
+        $client = $clientManager->createClient();
+        $client->setName($data['name']);
+        $client->setRedirectUris(array($data['redirect_uri']));
+        $client->setAllowedGrantTypes(array('authorization_code'));
+        $client->setUser($this->get('security.context')->getToken()->getUser());
+        $clientManager->updateClient($client);
+
     }
 }
--- src/Acme/DemoBundle/Entity/User.php
+++ src/Acme/DemoBundle/Entity/User.php
@@ -1,6 +1,12 @@
 <?php
 namespace Acme\DemoBundle\Entity;
 
+use Acme\DemoBundle\Entity\OAuth\AccessToken;
+
+use Acme\DemoBundle\Entity\OAuth\Client;
+
+use Doctrine\Common\Collections\ArrayCollection;
+
 use FOS\UserBundle\Entity\User as BaseUser;
 use Doctrine\ORM\Mapping as ORM;
 
@@ -12,7 +18,7 @@ use JMS\SerializerBundle\Annotation\Expose;
  * @ORM\Table(name="fos_user")
  * @ExclusionPolicy("all")
  */
- class User extends BaseUser
+class User extends BaseUser
 {
     /**
      * @ORM\Id
@@ -20,11 +26,75 @@ use JMS\SerializerBundle\Annotation\Expose;
      * @ORM\GeneratedValue(strategy="AUTO")
      */
     protected $id;
-    
-    
+
     /**
      * @Expose
-     */    
+     */
     protected $username;
 
+    /**
+     * @ORM\OneToMany(targetEntity="Acme\DemoBundle\Entity\OAuth\Client", mappedBy="user")
+     */
+    protected $clients;
+
+    /**
+     * @ORM\OneToMany(targetEntity="Acme\DemoBundle\Entity\OAuth\AccessToken", mappedBy="user")
+     */
+    protected $authorizedApps;
+
+    public function __construct()
+    {
+        parent::__construct();
+        $this->clients = new ArrayCollection();
+        $this->authorizedApps = new ArrayCollection();
+    }
+
+    /**
+     * @return Client[]
+     */
+    public function getClients()
+    {
+        return $this->clients;
+    }
+
+    /**
+     * @param ArrayCollection $clients
+     * @return \Acme\DemoBundle\Entity\User
+     */
+    public function setClients($clients)
+    {
+        $this->clients = $clients;
+        return $this;
+    }
+
+    /**
+     * @return boolean
+     */
+    public function hasClient()
+    {
+        return count($this->clients) > 0;
+    }
+
+    public function getClient()
+    {
+        return $this->clients[0];
+    }
+
+    /**
+     * @return AccessToken[]
+     */
+    public function getAuthorizedApps()
+    {
+        return $this->authorizedApps;
+    }
+
+    /**
+     * @param ArrayCollection $authorizedApps
+     * @return \Acme\DemoBundle\Entity\User
+     */
+    public function setAuthorizedApps($authorizedApps)
+    {
+        $this->authorizedApps = $authorizedApps;
+        return $this;
+    }
 }
